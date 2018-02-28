8a5dc232e8212423064330e24a61090ad2558a5d
Adam Kupczyk <akupczyk@redhat.com>
2018-02-28 11:20:26 +0100

    Added latency measurement
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

--- cgroup.cpp
+++ cgroup.cpp
@@ -18,6 +18,7 @@
 #include <assert.h>
 #include <sys/mman.h>
 #include <poll.h>
+#include <atomic>
 
 std::pair<bool, std::string> cgroup_memory_get()
 {
@@ -59,28 +60,6 @@ uint64_t get_usage(int fd)
   return strtol(data, nullptr, 0);
 }
 
-uint64_t notification_limit;
-std::list<std::pair<void*,size_t>> memory;
-std::mutex memory_lock;
-
-void allocating()
-{
-  while(true) {
-    usleep(10*1000);
-    std::cout << "Before: " << std::fixed << std::setprecision(6) << tnow() << std::endl;
-    uint64_t size = ((rand() % (notification_limit / 50) ) & ~0xfff) + 0x1000 ;
-    //std::cout << size << std::endl;
-    void* ptr = mmap(nullptr, size, PROT_READ|PROT_WRITE,
-                       MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
-    std::cout << ptr << ":" << size << std::endl;
-    
-    memset((char*)ptr, 0, size);
-    memory_lock.lock();
-    memory.push_back({ptr, size});
-    memory_lock.unlock();
-    std::cout << "After:  " << tnow() << std::endl;    
-  }
-}
 
 class memory_monitor
 {
@@ -122,11 +101,6 @@ public:
     return r>0;    
   }
 
-private:
-  int event_fd;
-  int m_usage_fd;
-  bool is_high;
-  
   static memory_monitor* create(uint64_t high_level) {
     static auto cgroup_memory = cgroup_memory_get();
     if (!cgroup_memory.first)
@@ -154,15 +128,47 @@ private:
       close (control_fd);
     return nullptr;
   }
+
+private:
+  int event_fd;
+  int m_usage_fd;
+  bool is_high;
 };
+
+
+memory_monitor* monitor;
+uint64_t notification_limit;
+std::list<std::pair<void*,size_t>> memory;
+std::mutex memory_lock;
+std::atomic<double> operation_time;
+std::atomic<bool> allocating_stop;
+
+void allocating()
+{
+  do {
+    uint64_t size = notification_limit / 50; //2%
+    if (size > 1024*1024)
+      size = 1024*1024;
+    size = ((rand() % size) & ~0xfff) + 0x1000 ;
+    void* ptr = mmap(nullptr, size, PROT_READ|PROT_WRITE,
+		     MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
+    operation_time.store(tnow());
+    memset((char*)ptr, 0, size);
+    std::cout << "Mem=" << monitor->get_current_level() << std::endl;
+    memory_lock.lock();
+    memory.push_back({ptr, size});
+    memory_lock.unlock();
+    usleep(10*1000);
+  } while (!allocating_stop.load());
+}
+
 
 int main(int argc, char** argv)
 {
-  char data[100];
-  int w;
   auto x = cgroup_memory_get();
   if (!x.first) {
     std::cerr << "No cgroups" << std::endl;
+    return -1;
   }
   std::string cgroup_memory = x.second;
   std::cout << "In cgroup memory: " << x.second << std::endl;
@@ -170,60 +176,42 @@ int main(int argc, char** argv)
   if (argc > 1) {
     notification_limit = strtoll(argv[1], nullptr, 0);
   } else {
-    int fd = open(("/sys/fs/cgroup/memory/"+cgroup_memory+"/memory.limit_in_bytes").c_str(), O_RDONLY);
+    int fd = open(("/sys/fs/cgroup/memory/"+cgroup_memory+"/memory.usage_in_bytes").c_str(), O_RDONLY);
     if (fd < 0) return -1;
-    w = read(fd, data, 99);
+    char data[100];
+    int w  = read(fd, data, 99);
     data[w] = 0;
     close (fd);
-    notification_limit = strtoll(data, nullptr, 0) / 100 * 95; //95%
+    notification_limit = strtoll(data, nullptr, 0) + 10*1024*1024;
   }
   std::cout << "Notification level: " << notification_limit << std::endl;
-  
-  int event_fd = eventfd(0, 0);
-  int m_usage_fd = open(("/sys/fs/cgroup/memory/"+cgroup_memory+"/memory.usage_in_bytes").c_str(), O_RDONLY);
-  int control_fd = open(("/sys/fs/cgroup/memory/"+cgroup_memory+"/cgroup.event_control").c_str(), O_WRONLY);
-  if (event_fd < 0 || m_usage_fd < 0 || control_fd < 0)
-    return -1;
-  w = sprintf(data, "%d %d %ld", event_fd, m_usage_fd, notification_limit);
-  if (write(control_fd, data, w) <= 0)
-    return -1;
-  close(control_fd);
 
+  monitor = memory_monitor::create(notification_limit);
   std::thread t1(allocating);
-  
-  while (true) {
-    uint64_t current_level;
-    eventfd_t event;
-    if (read(event_fd, &event, 8) != 8)
-      break;
-    std::cout << "event=" << event << std::endl;
-    //fcntl(event_fd, F_SETFD, (fcntl(event_fd, F_GETFD, 0)|O_NONBLOCK));
-    
-    std::cout << "Current level: " << get_usage(m_usage_fd) << " now=" << tnow() << std::endl;
-    //if (current_level > notification_limit)
-    do {
-      //if (current_level > notification_limit)
-      {
-	assert(memory.size()>=0);
-	auto ptr = memory.front();
-	std::cout << "Free " << ptr.first << std::endl;
-	memory.pop_front();
-	munmap(ptr.first, ptr.second);
-      }
-      std::cout << __LINE__ << std::endl;
-      struct pollfd poll_fd;
-      poll_fd.fd = event_fd;
-      poll_fd.events = POLLIN;
-      if (poll(&poll_fd, 1, 0) > 0) {
-	std::cout << "signalled" << std::endl;
-	 eventfd_t event;
-	 //read(event_fd, &event, 8);
-	 usleep(200*1000);
+  double latency_sum = 0;
+  int latency_samples = 0;
+
+  double start_time = tnow();
+  while (tnow() - start_time < 10) {
+    if (!monitor->is_level_high())
+      if (!monitor->wait_change(-1))
 	break;
-      }
-      std::cout << __LINE__ << std::endl;
-    } while (true);//(current_level > notification_limit);
-    //fcntl(event_fd, F_SETFD, (fcntl(event_fd, F_GETFD)&~O_NONBLOCK));
+    double latency = tnow() - operation_time.load();
+    std::cout << "trigger latency=" << std::fixed << std::setprecision(6) << latency << std::endl;
+    latency_sum += latency;
+    latency_samples ++;
+    while (monitor->is_level_high())
+    {
+      assert(memory.size()>=0);
+      auto ptr = memory.front();
+      memory.pop_front();
+      munmap(ptr.first, ptr.second);
+      monitor->wait_change(0);
+    };
   }
+  allocating_stop.store(true);
+  t1.join();
+  std::cout << "Average reaction latency: " <<
+	    std::fixed << std::setprecision(6) << latency_sum / latency_samples << std::endl;
   return -1;
 }
